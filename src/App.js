d7b1f6f6c873a64d0aab70156fa4b6139f04699c
Dinesh Jha <dineshkj@imspl.com>
2024-09-12 07:37:05 +0530

    made https call instead of http

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 import MovieDetails from './MovieDetails';
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
